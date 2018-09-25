77ffd60ea1398a1ad216a4cc70e13d66a3bd38ec
YurihOI <yuribarbosa.contato@gmail.com>
2018-09-25 14:04:31 -0300

    show roles on console

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
+from discord.utils import get
 import os
 import asyncio
 import time
 import random
 
+
 #Client = discord.Client()
 client = commands.Bot(command_prefix='!')
+server = discord.Server
 
 
 @client.event
@@ -15,9 +18,11 @@ async def on_ready():
     print("Bot is Ready")
 
 
-#@client.command(pass_context = True)
-#async def clear
-
+@client.command(pass_context = True)
+async def roles(ctx):
+    server = client.get_server('493970833799249930')
+    for r in server.roles:
+        print(r)
     
 @client.event
 async def on_message(message):
@@ -29,6 +34,10 @@ async def on_message(message):
     if message.content.upper() == "YUN TIME":
         await client.send_message(message.channel,"yun yun yun yun yun",tts = True)
     await client.process_commands(message)
-
+    
+@client.event
+async def on_member_join(member):
+    role = discord.utils.get(member.server.roles, name='Apprentice')
+    await client.add_roles(member, role)
 
 client.run(os.environ['BOT_TOKEN'])
