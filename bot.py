dee5a61c73b3da2abd5a26a63956f3657758fc72
YurihOI <yuribarbosa.contato@gmail.com>
2018-09-25 00:16:18 -0300

    Event?

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import asyncio
 import time
 import random
 
-Client = discord.Client()
+#Client = discord.Client()
 client = commands.Bot(command_prefix='!')
 
 
@@ -14,19 +14,10 @@ client = commands.Bot(command_prefix='!')
 async def on_ready():
     print("Bot is Ready")
 
-@client.event
-async def on_message(message):
-    if message.content.upper() == "MEGUMIN, CAST A SPELL!":
-        explosionImg = "https://cgtranslations321782266.files.wordpress.com/2018/03/megumin_kono_subarashii_sekai_ni_shukufuku_wo_and_uchi_no_hime_sama_ga_ichiban_kawaii__cafe24e947a0ff3592f2f13f96b81449.png"
-        embed = discord.Embed()
-        embed.set_image(url = explosionImg)
-        await client.send_message(message.channel,"EXPLOOOOOOOOOOOOOOOOSION", embed = embed)
-    if message.content.upper() == "YUN TIME":
-        await client.send_message(message.channel,"yun yun yun yun yun",tts = True)
 
 @client.command()
-async def ping(ctx):
-    await ctx.say('Pong')
+async def ping():
+    await client.say('Pong')
 
 
 #client.run('NDkzODc3MTIzNTU2MTc5OTg5.DorX7w.Q4nJBDz2UzUm9FvGJtZpSN4U-AY')
